adcb7aba9b9315797bf24625ce9f4349c7163178
Catherine Burns <catherineburns8@gmail.com>
2021-10-13 09:38:00 -0400

    small bug

--- config/website.js
+++ config/website.js
@@ -2,25 +2,26 @@ const meta = {
   // Metadata
   siteTitle: 'Catherine Burns',
   siteDescription:
-    'Catherine Burns',
+    'Anurag Hazra - Creative frontEnd web developer who loves javascript and modern web technologies.',
   siteTitleAlt: 'Catherine Burns',
   siteShortName: 'Catherine Burns',
   siteUrl: 'https://catherineburns.netlify.app', // No trailing slash!
 };
 
 const social = {
-  siteLogo: `src/static/CBLogo.png`,
+  siteLogo: `src/static/CBLogo.`,
   siteBanner: `${meta.siteUrl}/images/social-banner.png`,
+  twitter: '@anuraghazru',
 };
 
 const website = {
   ...meta,
   ...social,
-  disqusShortName: 'CatherineBurns',
+  disqusShortName: 'anuraghazra',
   googleAnalyticsID: 'UA-119972196-1',
   // Manifest
-  themeColor: '#8fb592',
-  backgroundColor: '#8fb592',
+  themeColor: '#6D83F2',
+  backgroundColor: '#6D83F2',
 };
 
 module.exports = website;

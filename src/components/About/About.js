ff6b2414d53d50f70c56c0b111225581cfc84530
Catherine Burns <69866375+cab40@users.noreply.github.com>
2022-01-09 21:30:15 -0500

    :-)

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -26,11 +26,11 @@ const About = () => {
           </a>{' '}
         to it!
         <br/> <br/> 
-        Glad you could make it! I’m Catherine, a Systems Design Engineering Student at the University of Waterloo (graduating class of 2025).
+        Glad you could make it! I’m Catherine, a <a target='_blank' href='https://uwaterloo.ca/systems-design-engineering/'>Systems Design Engineering</a> Student at the University of Waterloo (graduating class of 2025).
         <br/><br/>
-        Now you may be asking “what exactly is <a target='_blank' href='https://uwaterloo.ca/systems-design-engineering/'>Systems Design Engineering</a>?”, and I’m glad you asked since my class is wondering the same thing. 
-        What I can tell you, though, is that I’m a designer and dev with interests in UI/UX, software, HMI, and vehicle automation.
-        For the time being, I’m working as a Full Stack Software Engineer at Cisco, designing a Telemetry Dashboard for uWaterloo’s Midnight Sun Solar Car Team, 
+        I’m a designer and dev with interests in UI/UX, software, HMI, and vehicle automation. As a Systems Design Engineering student, I am developing expertise in using software and user-centred design to solve complex problems, optimize information systems and products. 
+        I interned at Cisco as a Full Stack Software Engineer and at Ford as an HMI Software Developer. Beyond my engineering, I practice visual arts (drawing, painting and graphic design) which has a positive influence on my creative mindset, inspiring me to find more innovative solutions.
+        For the time being, I’m volunteering as a Software Developer at <a target='_blank' href='https://developforgood.org/'>Develop For Good</a> where I'm working on a mobile application for Unicef, I am also designing a Telemetry Dashboard for uWaterloo’s Midnight Sun Solar Car Team, 
         trying out plenty of new recipes, and refining my spotify playlists! 
 
         </p>
